f09b9492870f1f543e794410a47779c7f1fbf953
Nelu_17 <chauhannilay888@gmail.com>
2026-03-26 09:17:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -70,9 +70,9 @@ with st.sidebar:
       files = [f for f in os.listdir("tutorial_PNGs") if f.endswith(".png")]
       for i in range(0, len(files), 4):
         cols = st.columns(4)
-          for j, col in enumerate(cols):
-            if i+j < len(files):
-              col.image(Image.open(os.path.join("tutorial_PNGs", files[i+j])), use_container_width=True)
+        for j, col in enumerate(cols):
+          if i+j < len(files):
+            col.image(Image.open(os.path.join("tutorial_PNGs", files[i+j])), use_container_width=True)
     
     uploaded_file = st.file_uploader("Upload Dataset (CSV, Excel, JSON)", type=["csv", "xlsx", "xls", "json"])
     
